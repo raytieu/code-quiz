8a538b8b36713f56b607554f3f23e0a2e6aa50c7
Ray Tieu <ray.tieu@gmail.com>
2020-07-05 18:15:34 -0700

    created functioning timer

--- assets/questions.js
+++ assets/questions.js
@@ -1,11 +1,29 @@
-var quizBtn = document.querySelector("#quiz-button");
-var hideFront = document.querySelector("#front-page");
-var visible1 = document.querySelector("#timer");
-var visible2 = document.querySelector("#questions");
+let quizBtn = document.querySelector("#quiz-button");
+let hideFront = document.querySelector("#front-page");
+let timeCounter = document.querySelector("#timer");
+let questionPage = document.querySelector("#questions");
 
 quizBtn.addEventListener("click", function() {
     hideFront.setAttribute("style", "display: none;");
-    visible1.classList.remove("invisible");
-    visible2.classList.remove("invisible");
-    
+    timeCounter.classList.remove("invisible");
+    questionPage.classList.remove("invisible"); 
+    startTimer();
 });
+
+function startTimer() {
+    let timeLeft = document.querySelector("#time-left");
+    let time = 10;
+    timeLeft.textContent = time;
+    
+    let countdown = setInterval(function() {
+        time--;
+        timeLeft.textContent = time;
+        
+        if (time === 0) {
+            clearInterval(countdown);
+        }
+    }, 1000)
+    
+
+       
+}
